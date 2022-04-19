e0a255de43e0e30f03d53999f3b4e22019779f39
Fukurou <caos2804@gmail.com>
2022-04-19 18:47:29 -0400

    refactoring code
    moved and created a bunch of files to make threading and generation code lighter

--- LandmassGen/Assets/Scripts/MapGenerator.cs
+++ LandmassGen/Assets/Scripts/MapGenerator.cs
@@ -20,16 +20,10 @@ public class MapGenerator : MonoBehaviour {
 
 	public Material terrainMaterial;
 
-
-
-
 	[Range(0, MeshSettings.numSupportedLODs-1)]
 	public int editorLODPreview;
 
 	public bool autoUpdate;
-
-	Queue<MapThreadInfo<HeightMap>> mapDataThreadInfoQueue = new Queue<MapThreadInfo<HeightMap>>();
-	Queue<MapThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>>();
 
 	float[,] fallOffMap;
 
@@ -44,12 +38,6 @@ public class MapGenerator : MonoBehaviour {
 	void OnTextureValuesUpdated()
 	{
 		textureData.ApplyToMaterial(terrainMaterial);
-	}
-
-	void Start()
-	{
-		textureData.ApplyToMaterial(terrainMaterial);
-		textureData.UpdateMeshHeights(terrainMaterial, heightMapSettings.minHeight, heightMapSettings.maxHeight);
 	}
 
 	public void DrawMapInEditor() {
@@ -77,52 +65,6 @@ public class MapGenerator : MonoBehaviour {
 		}
 	}
 
-	public void RequestHeightMap(Vector2 center, Action<HeightMap> callback) {
-		ThreadStart threadStart = delegate {
-			HeightMapThread (center, callback);
-		};
-
-		new Thread (threadStart).Start ();
-	}
-
-	void HeightMapThread(Vector2 center, Action<HeightMap> callback) {
-		HeightMap heightMap = HeightMapGenerator.GenerateHeightMap (meshSettings.numVertsPerLine, meshSettings.numVertsPerLine, heightMapSettings, center);
-		lock (mapDataThreadInfoQueue) {
-			mapDataThreadInfoQueue.Enqueue (new MapThreadInfo<HeightMap> (callback, heightMap));
-		}
-	}
-
-	public void RequestMeshData(HeightMap heightMap, int lod, Action<MeshData> callback) {
-		ThreadStart threadStart = delegate {
-			MeshDataThread (heightMap,  lod, callback);
-		};
-
-		new Thread (threadStart).Start ();
-	}
-
-	void MeshDataThread(HeightMap heightMap, int lod, Action<MeshData> callback) {
-		MeshData meshData = MeshGenerator.GenerateTerrainMesh (heightMap.values, meshSettings, lod);
-		lock (meshDataThreadInfoQueue) {
-			meshDataThreadInfoQueue.Enqueue (new MapThreadInfo<MeshData> (callback, meshData));
-		}
-	}
-
-	void Update() {
-		if (mapDataThreadInfoQueue.Count > 0) {
-			for (int i = 0; i < mapDataThreadInfoQueue.Count; i++) {
-				MapThreadInfo<HeightMap> threadInfo = mapDataThreadInfoQueue.Dequeue ();
-				threadInfo.callback (threadInfo.parameter);
-			}
-		}
-
-		if (meshDataThreadInfoQueue.Count > 0) {
-			for (int i = 0; i < meshDataThreadInfoQueue.Count; i++) {
-				MapThreadInfo<MeshData> threadInfo = meshDataThreadInfoQueue.Dequeue ();
-				threadInfo.callback (threadInfo.parameter);
-			}
-		}
-	}
-
 	void OnValidate() {
 
 		if (meshSettings != null)
@@ -142,18 +84,6 @@ public class MapGenerator : MonoBehaviour {
 			textureData.OnValuesUpdated -= OnTextureValuesUpdated;
 			textureData.OnValuesUpdated += OnTextureValuesUpdated;
 		}
-	}
-
-	struct MapThreadInfo<T> {
-		public readonly Action<T> callback;
-		public readonly T parameter;
-
-		public MapThreadInfo (Action<T> callback, T parameter)
-		{
-			this.callback = callback;
-			this.parameter = parameter;
-		}
-		
 	}
 
 }
